75ecc9185532c21204eac1b11b945afc493fb781
leoadvance <leo_advance@163.com>
2018-11-25 23:45:25 +0800

    增加串口号查找功能

--- startUARTLog.py
+++ startUARTLog.py
@@ -2,9 +2,12 @@
 # 允许带执行参数
 import getopt
 import sys
+import os
 print("start get UART log!")
 
-
+# UART 设备路径
+DEVICE_PATH = "/dev" 
+DEVICE_UART_KEY_WORD = "tty"
 #  系统参数
 sysPara = []
 
@@ -20,6 +23,22 @@ def usage():
     print("-p   UART Port number")
     print("-b   UART bitrate Default value is 115200")
 
+#   匹配文件并返回路径
+def findfile(path, name):
+    # 遍历目录
+    for relpath, dirs, files in os.walk(path): 
+
+        for filename in files:
+
+            # 文件名包含name 且包含 UART关键字
+            if (filename.find(name) != -1) and (filename.find(DEVICE_UART_KEY_WORD) != -1):
+                full_path = os.path.join(path, relpath, filename)
+                portPath = os.path.normpath(os.path.abspath(full_path))
+                #print(portPath)
+                return portPath
+
+        print("Can't find UART! err!")
+        sys.exit(1) 
 # 获取系统参数并解析
 def getSysPara():
     try:
@@ -30,9 +49,18 @@ def getSysPara():
             if o in ("-h", "--help"):
                 usage()
                 sys.exit(0)
+            # 提取波特率    
             if o in ("-b"):
                 UARTbitrate = a
                 print("bitrate: ", UARTbitrate)
+
+            # 提取端口号
+            if o in ("-p"):
+                #  for name in os.listdir("/dev"):
+                #     print(name)
+                UARTport = findfile(DEVICE_PATH, a)
+                print("UARTportPath: ", UARTport)
+
     except getopt.GetoptError as err:
         print('ERROR:', err)
         sys.exit(1)
